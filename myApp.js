80b3ad05706377f71463cd33ad049c7039239c10
Mejba Hasan <133997648+codermejba@users.noreply.github.com>
2024-04-03 16:41:47 +0600

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
+let port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     console.log('Hello World!');
 })
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
+})
+
 
 
 
